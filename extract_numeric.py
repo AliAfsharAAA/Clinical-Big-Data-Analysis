c99584dd6abf71f7b92899eb515160d113b9cd67
jastner109 <jastner109@gmail.com>
2018-06-03 20:16:24 -0400

    Add files via upload
    This extracts numeric data file (with extention *n.dat). It currently only creates subdirectories that contain data, however can be changed to include all subdirectories (comment in code).

---
+++ extract_numeric.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+
+working_dir = os.popen("pwd").read().split()[0]
+
+data_path = working_dir + "/database/mimic3wdb/matched"
+os.popen("mkdir " + working_dir + "/database/mimic3wdb/matched_numeric")
+numeric_path = working_dir + "/database/mimic3wdb/matched_numeric"
+
+level1 = os.popen("ls "+ data_path).read().split()
+
+for l1 in level1:
+	os.popen("mkdir " + numeric_path + "/" + l1)
+	level2 = os.popen("ls " + data_path + "/" + l1).read().split()
+	
+	for l2 in level2:
+		num_files = os.popen("ls " + data_path + "/" + l1 + "/" + l2 + "/*n.dat").read().split()
+			
+		if len(num_files) != 0:
+			os.popen("mkdir " + numeric_path + "/" + l1 + "/" + l2) # includes only folders that contain data, if wanna nclude all folders, place before if condition
+			os.popen("cp " + data_path + "/" + l1 + "/" + l2 + "/*n.dat " + numeric_path + "/" + l1 + "/" + l2)
