8e6ce54a0f02b64a5ee339f300aa70d51112ec2f
jastner109 <jastner109@gmail.com>
2018-06-10 20:23:22 -0400

    Minor updates to improve usability and for robustness.

--- getsignal.py
+++ getsignal.py
@@ -16,10 +16,11 @@ size = MPI.COMM_WORLD.Get_size()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-sid", "--signal_id", nargs="+", default=[], help="Input (different versions of) the name of signal to be recorded. Example: -sid \"ABP Mean\" \"ABP MEAN\"")
-parser.add_argument("-f", "--sampfrom", type=int, default=0, help="Input the beginning of time series sequence. Example: -f 100")
-parser.add_argument("-t", "--sampto", type=int, default=-1, help="Input the end of time series sequence. -1 for end of sequence. Example: -t 1000")
+parser.add_argument("-sf", "--sampfrom", type=int, default=0, help="Input the beginning of time series sequence. Example: -sf 100")
+parser.add_argument("-st", "--sampto", type=int, default=-1, help="Input the end of time series sequence. -1 for end of sequence. Example: -st 1000")
 parser.add_argument("-p", "--physical", type=bool, default=True, help="Input whether signal is physical or digital. Example: -p 0 OR -p True")
-parser.add_argument("-s", "--save", type=bool, default=False, help="Save or not save data in csv. Example: -s 1 OR -s False")
+parser.add_argument("-smd", "--savemetadata", type=bool, default=False, help="Save or not save metadata in csv. Example: -smd 1 OR -smd False")
+parser.add_argument("-sts", "--savetimeseries", type=bool, default=False, help="Save or not save timeseries data in csv. Example: -sts 1 OR -sts False")
 args = vars(parser.parse_args())
 
 
@@ -34,23 +35,35 @@ else:
 filename1 = work_dir + "/processed_data/metadata/" + "-".join(signal_id).replace(" ", "_") + "_metadata" + ".csv"
 filename2 = work_dir + "/processed_data/timeseries/" + "-".join(signal_id).replace(" ", "_") + "_timeseries" + ".csv"
 
-if args["save"]:
-    if rank == 0:
-    
+if rank == 0:
+        
+    if args["savemetadata"]:
+            
         os.system("mkdir -p " +  work_dir + "/processed_data/metadata")
-        os.system("mkdir -p " +  work_dir + "/processed_data/timeseries")
-
+            
         f1 = open(filename1, "a")
         writer = csv.writer(f1)
         writer.writerow(col_names)
         f1.close()
+            
+    if args["savetimeseries"]:
+            
+        os.system("mkdir -p " +  work_dir + "/processed_data/timeseries")
+            
+        col_names2 = col_names[:2]
+        col_names2.append("time_series_sequence")
+        
         f2 = open(filename2, "a")
         writer = csv.writer(f2)
-        writer.writerow(col_names[:2])
+        writer.writerow(col_names2)
         f2.close()
 
 
 for dr in dirs:
+    
+    records_entry = list()
+    records_data = list()
+    
     if dirs.index(dr)%size != rank: continue
     
     else:
@@ -59,7 +72,7 @@ for dr in dirs:
         level2 = os.popen("ls " + level1).read().split()
         for l2 in level2:
             level3 = os.popen("ls " + level1 + "/" + l2 + "/" + "p*n.hea").read().split()
-
+            
             for l3 in level3:
 
                 record = wfdb.io.rdrecord(
@@ -86,8 +99,6 @@ for dr in dirs:
 
                     record_entry.append(record["record_name"].split("-")[0])     # patient id for metadata csv
                     record_entry.append(str(record["base_date"]) + "_" + "-".join(str(record["base_time"]).split(":")))     # record datetime for metadata csv
-                    record_data.append(record["record_name"].split("-")[0])     # patient id for time series csv
-                    record_data.append(str(record["base_date"]) + "_" + "-".join(str(record["base_time"]).split(":")))     # record datetime for time series csv
                     if not record["comments"]: record_entry.append("n/a")     # location
                     else: record_entry.append(record["comments"][0].split(": ")[1])
                     record_entry.append(record["n_sig"])     # number of signals
@@ -95,6 +106,10 @@ for dr in dirs:
                     if not record["counter_freq"]: record_entry.append("n/a")     # counter frequency
                     else: record_entry.append(record["counter_freq"])
                     record_entry.append(record["sig_len"])     # signal length
+                    
+                    record_data.append(record["record_name"].split("-")[0])     # patient id for time series csv
+                    record_data.append(str(record["base_date"]) + "_" + "-".join(str(record["base_time"]).split(":")))     # record datetime for time series csv
+                    
                     if record["p_signal"] is None:     # signal type
                         record_entry.append("digital")
                         record_data.extend(list(record["d_signal"].T[signal_id_index]))     # signal time series data
@@ -102,14 +117,18 @@ for dr in dirs:
                         record_entry.append("physical")
                         record_data.extend(list(record["p_signal"].T[signal_id_index]))     # signal time series data
                     record_entry.append(record["units"][signal_id_index])     # signal unit
-
-                    if args["save"]:
-
-                        f1 = open(filename1, "a")
-                        writer = csv.writer(f1)
-                        writer.writerow(record_entry)
-                        f1.close()
-                        f2 = open(filename2, "a")
-                        writer = csv.writer(f2)
-                        writer.writerow(record_data)
-                        f2.close()
+                    
+                    records_entry.append(record_entry)
+                    records_data.append(record_data)
+                    
+        if args["savemetadata"]:
+            f1 = open(filename1, "a")
+            writer = csv.writer(f1)
+            writer.writerows(records_entry)
+            f1.close()
+                
+        if args["savetimeseries"]:                    
+            f2 = open(filename2, "a")
+            writer = csv.writer(f2)
+            writer.writerows(records_data)
+            f2.close()
