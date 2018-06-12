2d4ec1690a8a935c216cc6a442354472aad1534b
arcrank <arcrank@gmail.com>
2018-06-12 15:53:14 -0400

    Update extract_numeric_2.py

--- extract_numeric_2.py
+++ extract_numeric_2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 import os
 import shutil
-
-
-
 import argparse
+
+
+
+# add folder argument in so as we can run them independtly across the 10 folders
 parser = argparse.ArgumentParser()
 parser.add_argument('folder',type = str,help = "folder to copy")
 arg = parser.parse_args()
@@ -23,13 +24,14 @@ folder_path = os.path.join(copy_from_path,folder)
 
 patient_records = os.listdir(folder_path)
 
+#Iterate over all the folders 
 for patient_record in patient_records:
 
     from_location = os.path.join(folder_path,patient_record)
-
+    #Finds the numeric files
     records = [file for file in os.listdir(from_location) if file.endswith('n.hea') or file.endswith('n.dat')]
 
-
+    #If there are numeric files, creates the directory in new location to copy files to
     if records != []:
 
         destination_path = os.path.join(copy_to_path,patient_record)
@@ -39,5 +41,8 @@ for patient_record in patient_records:
 
         for record in records:
             full_file_name = os.path.join(from_location,record)
-            shutil.copy(full_file_name,)
+
+            #copy files
+            if (os.path.isfile(full_file_name)):
+                shutil.copy(full_file_name, destination_path)
 
