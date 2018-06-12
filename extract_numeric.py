3171c4c8e5e7418bc672cbbdb16c06c51ce2584f
jastner109 <jastner109@gmail.com>
2018-06-12 18:52:14 -0400

    Much more efficient way to extract the numeric files.
    - no loops
    - checks if all files have been copied correctly or not.

--- extract_numeric.py
+++ extract_numeric.py
@@ -1,22 +1,19 @@
-#!/usr/bin/python
-
 import os
 
-working_dir = os.popen("pwd").read().split()[0]
+database_dir = "/".join(os.popen("pwd").read().split()[0].split("/")[:-1])
 
-data_path = working_dir + "/database/mimic3wdb/matched"
-os.popen("mkdir " + working_dir + "/database/mimic3wdb/matched_numeric")
-numeric_path = working_dir + "/database/mimic3wdb/matched_numeric"
+data_path = database_dir + "/database/mimic3wdb/matched"
+numeric_path = database_dir + "/database/mimic3wdb/matched_numeric"
 
-level1 = os.popen("ls "+ data_path).read().split()
+os.system("rsync -ap --prune-empty-dirs --include '*/' --include '*n.*' --exclude '*' " + data_path + "/* " + numeric_path)
 
-for l1 in level1:
-	os.popen("mkdir " + numeric_path + "/" + l1)
-	level2 = os.popen("ls " + data_path + "/" + l1).read().split()
-	
-	for l2 in level2:
-		num_files = os.popen("ls " + data_path + "/" + l1 + "/" + l2 + "/*n.*").read().split()
-			
-		if len(num_files) != 0:
-			os.popen("mkdir " + numeric_path + "/" + l1 + "/" + l2) # includes only folders that contain data, if wanna nclude all folders, place before if condition
-			os.popen("cp " + data_path + "/" + l1 + "/" + l2 + "/*n.* " + numeric_path + "/" + l1 + "/" + l2)
+dhc = int(os.popen("find " + data_path + " -type f -name '*n.hea' | wc -l").read().split()[0])
+ddc = int(os.popen("find " + data_path + " -type f -name '*n.dat' | wc -l").read().split()[0])
+nhc = int(os.popen("find " + numeric_path + " -type f -name '*n.hea' | wc -l").read().split()[0])
+ndc = int(os.popen("find " + numeric_path + " -type f -name '*n.dat' | wc -l").read().split()[0])
+
+if dhc + ddc != nhc + ndc:
+	if dhc != nhc and ddc == ndc: print("Error in copying numeric header files.")
+	elif dhc == nhc and ddc != ndc: print("Error in copying numeric data files.")
+	elif dhc != nhc and ddc != ndc: print("Error in copying both numeric header and data files.")
+else: print("Numeric files copied successfully.")
