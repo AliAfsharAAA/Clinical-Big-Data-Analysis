9e1cafe1541de04c5872b71a84839b2b82a7deaa
jastner109 <jastner109@gmail.com>
2018-06-14 16:51:43 -0400

    Moved to new folder
    - changed numeric folder name

--- dsingh34/getsignaldata.py
+++ dsingh34/getsignaldata.py
@@ -7,7 +7,7 @@ import wfdb
 
 database_dir = "/".join(os.popen("pwd").read().split()[0].split("/")[:-1])
 work_dir = os.popen("pwd").read().split()[0]
-data_dir = "database/mimic3wdb/matched_numeric"
+data_dir = "database/mimic3wdb/matched_num"
 data_path = database_dir + "/" + data_dir
 dirs = [dr.split("/")[-2] for dr in os.popen("ls -d " + data_path + "/*/").read().split()]
 
