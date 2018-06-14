4200d116479dab6e54d433780508a1a59c59039b
jastner109 <jastner109@gmail.com>
2018-06-14 16:52:54 -0400

    Moved to nw folder
    - renamed numeric data folder

--- dsingh34/makenumset.py
+++ dsingh34/makenumset.py
@@ -5,7 +5,7 @@ import argparse
 database_dir = "/".join(os.popen("pwd").read().split()[0].split("/")[:-1])
 
 data_path = database_dir + "/database/mimic3wdb/matched"
-numeric_path = database_dir + "/database/mimic3wdb/matched_numeric"
+numeric_path = database_dir + "/database/mimic3wdb/matched_num"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--create", action="store_true", help="Create numeric dataset. Example: -c")
@@ -15,6 +15,7 @@ args = vars(parser.parse_args())
 if args["create"]:
 
 	os.system("rsync -ap --prune-empty-dirs --include '*/' --include '*n.*' --exclude '*' " + data_path + "/* " + numeric_path) # removes the empty directories. If you want to keep empty directories, remove the --prune-empty-dirs flag.
+	os.system("cp " + data_path + "/RECORDS-numerics " + numeric_path)
 	
 	dhc = len(os.popen("find " + data_path + " -type f -name '*n.hea'").read().split())
 	ddc = len(os.popen("find " + data_path + " -type f -name '*n.dat'").read().split())
