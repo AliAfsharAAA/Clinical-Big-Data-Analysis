8403f1b5fde1fe795d62bef2a052697ecda0369e
jastner109 <jastner109@gmail.com>
2018-06-15 10:18:26 -0400

    Check error made more efficient

--- dsingh34/makenumset.py
+++ dsingh34/makenumset.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import re
 
 
 database_dir = "/".join(os.popen("pwd").read().split()[0].split("/")[:-1])
@@ -14,16 +15,16 @@ args = vars(parser.parse_args())
 
 if args["create"]:
 
-	os.system("rsync -ap --prune-empty-dirs --include '*/' --include '*n.*' --exclude '*' " + data_path + "/* " + numeric_path) # removes the empty directories. If you want to keep empty directories, remove the --prune-empty-dirs flag.
 	os.system("cp " + data_path + "/RECORDS-numerics " + numeric_path)
+	os.system("rsync -ap --prune-empty-dirs --include '*/' --include '*n.*' --exclude '*' " + data_path + "/* " + numeric_path) # removes the empty directories. If you want to keep empty directories, remove the --prune-empty-dirs flag.
 	
-	dhc = len(os.popen("find " + data_path + " -type f -name '*n.hea'").read().split())
-	ddc = len(os.popen("find " + data_path + " -type f -name '*n.dat'").read().split())
-	nhc = len(os.popen("find " + numeric_path + " -type f -name '*n.hea'").read().split())
-	ndc = len(os.popen("find " + numeric_path + " -type f -name '*n.dat'").read().split())
+	num_orig_hea = set([re.split("/|\.", filename)[-2] for filename in os.popen("find " + data_path + " -type f -name '*n.hea'").read().split()])
+	num_made_hea = set([re.split("/|\.", filename)[-2] for filename in os.popen("find " + numeric_path + " -type f -name '*n.hea'").read().split()])
+	num_orig_dat = set([re.split("/|\.", filename)[-2] for filename in os.popen("find " + data_path + " -type f -name '*n.dat'").read().split()])
+	num_made_dat = set([re.split("/|\.", filename)[-2] for filename in os.popen("find " + numeric_path + " -type f -name '*n.dat'").read().split()])
 	
-	if dhc + ddc != nhc + ndc:
-		if dhc != nhc and ddc == ndc: print("Error in copying numeric header files.")
-		elif dhc == nhc and ddc != ndc: print("Error in copying numeric data files.")
-		elif dhc != nhc and ddc != ndc: print("Error in copying both numeric header and data files.")
+	if num_orig_hea != num_made_hea or num_orig_dat != num_made_dat:
+		if num_orig_hea != num_made_hea and num_orig_dat == num_made_dat: print("Error in copying numeric header files.")
+		elif num_orig_hea == num_made_hea and num_orig_dat != num_made_dat: print("Error in copying numeric data files.")
+		elif num_orig_hea != num_made_hea and num_orig_dat != num_made_dat: print("Error in copying both numeric header and data files.")
 	else: print("Numeric files only database created successfully.")
