86e8d064910126e0d1231bee7cdb21a3666c4f89
jastner109 <jastner109@gmail.com>
2018-07-20 12:09:10 -0400

    Delete receive.sh

--- dsingh34/logistics/receive.sh
+++
@@ -1,6 +0,0 @@
-ssh dsingh34@jhu.edu@gateway2.marcc.jhu.edu "zip -r ~/work2/dsingh34/transfers.zip ~/work2/dsingh34/processed_data"
-scp dsingh34@jhu.edu@gateway2.marcc.jhu.edu:~/work2/dsingh34/transfers.zip ~/Desktop/jhmi 
-ssh dsingh34@jhu.edu@gateway2.marcc.jhu.edu "rm ~/work2/dsingh34/transfers.zip"
-unzip transfers.zip
-mv home*/dsingh34@jhu.edu/work2/dsingh34/processed_data .
-rm -r transfers.zip home*
